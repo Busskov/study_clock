2b9cb0537811bbe94d897b751bf3fdebb21ee4cf
Artem Belous <belous.artem77@gmail.com>
2024-10-03 11:49:43 +0300

    Add user and activity models, add page for creating new user

---
+++ study_clock/clock/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+from clock.models import User
+
+# Create your views here.
+
+def index(response):
+    user = User(login="BolTss", password_hash="hash", date_of_birth ="15/10/2004", email="bol@gmail.com",
+                country="by", is_premium=False)
+    print("hello, this is ", user.login)
+    return HttpResponse('Clock is about to be here soon...\n'
+                        'In the meantime, you may think about buying a premium subscription to encourage further development')
