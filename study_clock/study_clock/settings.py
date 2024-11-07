6449fd5cc07158acadcbcd85e32118cf662be307
Artem Belous <belous.artem77@gmail.com>
2024-11-07 16:16:48 +0300

    Fixed settings

--- study_clock/study_clock/settings.py
+++ study_clock/study_clock/settings.py
@@ -58,10 +58,7 @@ CHANNEL_LAYERS = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
+    )
 }
 
 from datetime import timedelta
