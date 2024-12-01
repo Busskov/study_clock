c5d3afdec84ca0e1e9203c6fbbc5f2477a9cedb8
aadeglmmy <aadeglmy@gmail.com>
2024-12-01 23:10:47 +0300

    Added docker-compose.yml

--- study_clock/clock/routing.py
+++ study_clock/clock/routing.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
     path('ws/chat/<int:user_id>/', consumers.ChatConsumer.as_asgi()),
-    # re_path(r'ws/support/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/support/$', consumers.ChatConsumerSupport.as_asgi()),
 ]
